32f232e1a1219266a2998204b706b77fb5dedff0
KodanevAndrey <zig-zag2004@mail.ru>
2024-12-21 15:56:50 +0300

    Commit1

---
+++ TestConnectToDBeaver/TestConnectToDBeaver/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+using System.Reflection.Emit;
+using System.Data;
+
+namespace TestConnectToDBeaver
+{
+    public class DBHelper
+    {
+        private SQLiteConnection m_dbConn = new SQLiteConnection();
+        private SQLiteCommand m_sqlCmd = new SQLiteCommand();
+        private string FileLocation = "DBeaverTest.sqlite";
+        public void ConnectDB()
+        {
+            try
+            {
+                m_dbConn = new SQLiteConnection("Data Source=" + FileLocation + ";Version=3;");
+                m_dbConn.Open();
+                m_sqlCmd.Connection = m_dbConn;
+                Console.WriteLine("Connected!");
+            }
+            catch (SQLiteException ex)
+            {
+                Console.WriteLine("Error ConnectDB: " + ex.Message);
+            }
+        }
+
+        public void ReadDB()
+        {
+            /*
+            if (m_dbConn.State != ConnectionState.Open)
+            {
+                Console.WriteLine("Open connection with database");
+                return;
+            }
+            */
+
+            try
+            {
+                string connectionString = "Data Source=" + FileLocation + ";Version=3;";
+                Console.WriteLine("Connected!");
+                using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+                {
+                    connection.Open();
+
+                    // SQL-запрос для получения данных из связанных таблиц
+                    string query = @"
+                SELECT s.id, s.userName, s.city AS BasicTable, c.city
+                FROM BasicTable s
+                JOIN CityTable c ON s.city = c.id;";
+
+                    using (SQLiteCommand command = new SQLiteCommand(query, connection))
+                    {
+                        using (SQLiteDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                string userID = reader["id"].ToString();
+                                string userName = reader["userName"].ToString();
+                                string city = reader["city"].ToString();
+                                Console.WriteLine($"UserID: {userID}, userName: {userName}, City: {city}");
+                            }
+                        }
+                    }
+                }
+            }
+            catch (SQLiteException ex)
+            {
+                Console.WriteLine("Error ConnectDB: " + ex.Message);
+            }
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DBHelper db = new DBHelper(); 
+            //db.ConnectDB();
+            db.ReadDB();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TestConnectToDBeaver/TestConnectToDBeaver/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("TestConnectToDBeaver")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestConnectToDBeaver")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("7b495132-3c55-4fc9-8db1-220756fe4782")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
