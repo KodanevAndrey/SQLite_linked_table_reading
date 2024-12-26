688d35da49634d1a57dbde14d4b1e7d4d82970dd
KodanevAndrey <zig-zag2004@mail.ru>
2024-12-26 18:47:19 +0300

    AddNewTypeTable

--- TestConnectToDBeaver/TestConnectToDBeaver/Program.cs
+++ TestConnectToDBeaver/TestConnectToDBeaver/Program.cs
@@ -14,6 +14,48 @@ namespace TestConnectToDBeaver
         private SQLiteConnection m_dbConn = new SQLiteConnection();
         private SQLiteCommand m_sqlCmd = new SQLiteCommand();
         private string FileLocation = "DBeaverTest.sqlite";
+
+        public void ReadDB()
+        {
+            try
+            {
+                string connectionString = "Data Source=" + FileLocation + ";Version=3;";
+                Console.WriteLine("Connected!");
+                using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+                {
+                    connection.Open();
+
+                    // SQL-запрос для получения данных из связанных таблиц
+
+                    string query = @"
+                        SELECT a.id, a.userName, b.city, c.Type
+                        AS BasicTable, b.city, c.Type
+                        FROM BasicTable a
+                        JOIN CityTable b ON a.city = b.id
+                        JOIN TypeTable c ON a.Type = c.id
+                        ;";
+
+                    using (SQLiteCommand command = new SQLiteCommand(query, connection))
+                    {
+                        using (SQLiteDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                string userID = reader["id"].ToString();
+                                string userName = reader["userName"].ToString();
+                                string city = reader["city"].ToString();
+                                string type = reader["Type"].ToString();
+                                Console.WriteLine($"UserID: {userID}, userName: {userName}, City: {city}, Type: {type}");
+                            }
+                        }
+                    }
+                }
+            }
+            catch (SQLiteException ex)
+            {
+                Console.WriteLine("Error ConnectDB: " + ex.Message);
+            }
+        }
         public void ConnectDB()
         {
             try
@@ -28,58 +70,13 @@ namespace TestConnectToDBeaver
                 Console.WriteLine("Error ConnectDB: " + ex.Message);
             }
         }
-
-        public void ReadDB()
-        {
-            /*
-            if (m_dbConn.State != ConnectionState.Open)
-            {
-                Console.WriteLine("Open connection with database");
-                return;
-            }
-            */
-
-            try
-            {
-                string connectionString = "Data Source=" + FileLocation + ";Version=3;";
-                Console.WriteLine("Connected!");
-                using (SQLiteConnection connection = new SQLiteConnection(connectionString))
-                {
-                    connection.Open();
-
-                    // SQL-запрос для получения данных из связанных таблиц
-                    string query = @"
-                SELECT s.id, s.userName, s.city AS BasicTable, c.city
-                FROM BasicTable s
-                JOIN CityTable c ON s.city = c.id;";
-
-                    using (SQLiteCommand command = new SQLiteCommand(query, connection))
-                    {
-                        using (SQLiteDataReader reader = command.ExecuteReader())
-                        {
-                            while (reader.Read())
-                            {
-                                string userID = reader["id"].ToString();
-                                string userName = reader["userName"].ToString();
-                                string city = reader["city"].ToString();
-                                Console.WriteLine($"UserID: {userID}, userName: {userName}, City: {city}");
-                            }
-                        }
-                    }
-                }
-            }
-            catch (SQLiteException ex)
-            {
-                Console.WriteLine("Error ConnectDB: " + ex.Message);
-            }
-        }
     }
 
     internal class Program
     {
         static void Main(string[] args)
         {
-            DBHelper db = new DBHelper(); 
+            DBHelper db = new DBHelper();
             //db.ConnectDB();
             db.ReadDB();
             Console.ReadKey();
